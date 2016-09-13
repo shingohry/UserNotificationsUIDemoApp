732e36e8c2a1ae4fdc6f5a39d6555e58db460fbb
Shingo Hiraya <shingohiraya@gmail.com>
2016-09-13 21:31:40 +0900

    update FourthNotificationContentExtension's NotificationViewController

--- FourthNotificationContentExtension/NotificationViewController.swift
+++ FourthNotificationContentExtension/NotificationViewController.swift
@@ -12,15 +12,42 @@ import UserNotificationsUI
 
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
 
+    // MARK: - Properties
+    
     @IBOutlet var label: UILabel?
+    
+    var mediaPlayPauseButtonType: UNNotificationContentExtensionMediaPlayPauseButtonType {
+        get {
+            return .default
+        }
+    }
+    
+    var mediaPlayPauseButtonFrame: CGRect {
+        get {
+            return CGRect(x: 0.0, y: 0.0, width: 100.0, height: 50.0)
+        }
+    }
+    
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any required interface initialization here.
     }
     
+    // MARK: - UNNotificationContentExtension
+    
     func didReceive(_ notification: UNNotification) {
         self.label?.text = notification.request.content.body
     }
-
+    
+    func mediaPlay() {
+        print(Date(), #function, #line)
+        
+    }
+    
+    func mediaPause() {
+        print(Date(), #function, #line)
+        
+    }
 }
