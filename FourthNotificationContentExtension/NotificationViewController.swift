ae16ebf2c6d9f9995c36da3e419eaf46ed9b8ad4
Shingo Hiraya <shingohiraya@gmail.com>
2016-09-13 19:22:20 +0900

    add FourthNotificationContentExtension

---
+++ FourthNotificationContentExtension/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  FourthNotificationContentExtension
+//
+//  Created by 平屋真吾 on 2016/09/13.
+//  Copyright © 2016 Shingo Hiraya. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
